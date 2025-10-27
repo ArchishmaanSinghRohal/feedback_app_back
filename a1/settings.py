9c3535ec9ff60181dae7a9c45458d5e5de239b85
Archishmaan <archishmaanrohal@gmail.com>
2025-10-27 15:55:53 +0530

    Fixed settings

--- a1/settings.py
+++ a1/settings.py
@@ -48,7 +48,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
